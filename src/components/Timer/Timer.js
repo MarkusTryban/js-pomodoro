4c3884b4c2f41a45623a64ea675f11a19c1701d0
MarkusTryban <markustryban@gmail.com>
2020-08-02 22:09:01 -0400

    Create Timer component

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+      isOn: false,
+    };
+  }
+}
