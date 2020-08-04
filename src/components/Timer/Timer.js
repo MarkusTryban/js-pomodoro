93c6a68a248155016b68b2d08ffe25c936d3e0fd
MarkusTryban <markustryban@gmail.com>
2020-08-04 19:54:04 -0400

    Update startTimer func

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -20,9 +20,31 @@ class Timer extends Component {
   }
 
   startTimer() {
-    this.setState({
-      isOn: true,
-    });
+    const { isOn } = this.state;
+
+    if (isOn === true) {
+      return;
+    }
+    this.myInterval = setInterval(() => {
+      const { seconds, minutes } = this.state;
+
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1,
+        }));
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval);
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59,
+          }));
+        }
+      }
+    }, 1000);
+    this.setState({ isOn: true });
   }
 
   stopTimer() {
