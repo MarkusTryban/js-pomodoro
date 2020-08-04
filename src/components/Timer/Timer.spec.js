21f8e5d77703c3ef87d87340cd750a48cc59fe84
MarkusTryban <markustryban@gmail.com>
2020-08-04 19:34:23 -0400

    Reset button click set isOn to false

--- src/components/Timer/Timer.spec.js
+++ src/components/Timer/Timer.spec.js
@@ -58,4 +58,12 @@ describe('mounted Timer', () => {
     container.find('.stop-timer').first().simulate('click');
     expect(container.instance().state.isOn).toEqual(false);
   });
+
+  it('should change isOn state false when the reset button is clicked', () => {
+    container.instance().forceUpdate();
+    container.find('.reset-timer').first().simulate('click');
+    expect(container.instance().state.isOn).toEqual(false);
+    expect(container.instance().state.minutes).toEqual(25);
+    expect(container.instance().state.seconds).toEqual(0);
+  });
 });
