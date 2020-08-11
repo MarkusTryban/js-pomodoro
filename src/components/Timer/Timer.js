3d36a5b63eb240912218086dd387285f1b942e2b
MarkusTryban <markustryban@gmail.com>
2020-08-10 20:12:19 -0400

    Replace break-decrement with reg button

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -73,7 +73,7 @@ class Timer extends Component {
         <div id='break-label'>Break Length</div>
         <div id='session-label'>Session Length</div>
         <div className='timer-button-container'>
-          <TimerButton id='break-decrement' buttonValue='Break-dec' />
+          <button id='break-decrement' />
           <TimerButton
             className='start-timer'
             buttonAction={this.startTimer}
