9c6975843ae366d5fa1dbd30ad03d95520eeab42
MarkusTryban <markustryban@gmail.com>
2020-08-16 17:41:30 -0400

    Fix typo

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -109,7 +109,7 @@ class Timer extends Component {
           <i className='fa fa-arrow-down fa-2x' />
         </button>
         <div className='label'>
-          <span id='break-increment'>{breakLength}</span>
+          <span id='break-length'>{breakLength}</span>
         </div>
         <button id='break-increment' onClick={this.incrementBreak}>
           <i className='fa fa-arrow-up fa-2x' />
