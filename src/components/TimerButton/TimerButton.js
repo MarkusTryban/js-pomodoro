0e7491a7868cd4c912422bec234fdf4299e7317d
MarkusTryban <markustryban@gmail.com>
2020-08-22 20:37:04 -0400

    Add all button elements to props

--- src/components/TimerButton/TimerButton.js
+++ src/components/TimerButton/TimerButton.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './TimerButton.css';
 
-const TimerButton = () => (
-  <div className='button-container'>
-    <div id={labelID}>{label}</div>
-    <button
-      id={decrementID}
-      className='timer-button'
-      value='-'
-      onClick={onClick}
-    >
-      <i className='fa fa-arrow-down fa-2x' />
-    </button>
-    <div id={lengthID} className='timer-button'>
-      {length}
-    </div>
-    <button
-      id={incrementID}
-      className='timer-button'
-      value='+'
-      onClick={onClick}
-    >
-      <i className='fa fa-arrow-down fa-2x' />
-    </button>
-  </div>
-);
+class TimerButton extends Component {
+  render() {
+    const {
+      labelID,
+      label,
+      decrementID,
+      lengthID,
+      length,
+      incrementID,
+      handleClick,
+    } = this.props;
+
+    return (
+      <div>
+        <div id={labelID}>{label}</div>
+        <button
+          id={decrementID}
+          className='timer-button'
+          value='-'
+          onClick={() => handleClick()}
+        >
+          <i className='fa fa-arrow-down fa-2x' />
+        </button>
+        <div id={lengthID} className='timer-button'>
+          {length}
+        </div>
+        <button
+          id={incrementID}
+          className='timer-button'
+          value='+'
+          onClick={() => handleClick()}
+        >
+          <i className='fa fa-arrow-up fa-2x' />
+        </button>
+      </div>
+    );
+  }
+}
 
 export default TimerButton;
