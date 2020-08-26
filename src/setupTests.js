a9b7fad0149fc0237766b00381607c7d961d71ca
MarkusTryban <markustryban@gmail.com>
2020-08-26 16:58:08 -0400

    Extend JSDOM WITH HTMLMediaElement

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,3 +5,16 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+window.HTMLMediaElement.prototype.load = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.play = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.pause = () => {
+  /* do nothing */
+};
+window.HTMLMediaElement.prototype.addTextTrack = () => {
+  /* do nothing */
+};
