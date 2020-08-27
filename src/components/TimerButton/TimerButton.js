79dabbd21835ec171a7796e71f7d757103fd8951
MarkusTryban <markustryban@gmail.com>
2020-08-27 15:42:58 -0400

    Remove class from session length label

--- src/components/TimerButton/TimerButton.js
+++ src/components/TimerButton/TimerButton.js
@@ -25,9 +25,7 @@ class TimerButton extends Component {
         >
           <i className='fa fa-arrow-down fa-2x' />
         </button>
-        <div id={lengthID} className='timer-button'>
-          {length}
-        </div>
+        <div id={lengthID}>{length}</div>
         <button
           id={incrementID}
           className='timer-button'
