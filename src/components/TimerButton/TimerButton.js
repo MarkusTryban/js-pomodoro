75e8e08fb0dac6e4f0fbef0622fd281fb6c307a0
MarkusTryban <markustryban@gmail.com>
2020-09-16 18:31:36 -0400

    Change class to functional component

--- src/components/TimerButton/TimerButton.js
+++ src/components/TimerButton/TimerButton.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './TimerButton.css';
 
-class TimerButton extends Component {
-  render() {
-    const {
-      labelID,
-      label,
-      decrementID,
-      lengthID,
-      length,
-      incrementID,
-      onClick,
-    } = this.props;
+const TimerButton = (props) => {
+  const {
+    labelID,
+    label,
+    decrementID,
+    lengthID,
+    length,
+    incrementID,
+    onClick,
+  } = props;
 
-    return (
-      <div className='button-container'>
-        <div id={labelID}>{label}</div>
-        <button
-          id={decrementID}
-          className='timer-button'
-          value='-'
-          onClick={onClick}
-        >
-          <i className='fa fa-arrow-down fa-2x' />
-        </button>
-        <div id={lengthID} className='timer-button'>
-          {length}
-        </div>
-        <button
-          id={incrementID}
-          className='timer-button'
-          value='+'
-          onClick={onClick}
-        >
-          <i className='fa fa-arrow-up fa-2x' />
-        </button>
+  return (
+    <div className='button-container'>
+      <div id={labelID}>{label}</div>
+      <button
+        id={decrementID}
+        className='timer-button'
+        value='-'
+        onClick={onClick}
+      >
+        <i className='fa fa-arrow-down fa-2x' />
+      </button>
+      <div id={lengthID} className='timer-button'>
+        {length}
       </div>
-    );
-  }
-}
+      <button
+        id={incrementID}
+        className='timer-button'
+        value='+'
+        onClick={onClick}
+      >
+        <i className='fa fa-arrow-up fa-2x' />
+      </button>
+    </div>
+  );
+};
 
 export default TimerButton;
